88693bd076e5e8a77d86fbad307f04e25aa1b6b6
Lucia Morena <luciamorena77@gmail.com>
2025-02-24 17:26:40 -0300

    add tailwind 4 and action schedule

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ export default function App() {
       <h2>{subtrair(10, 2)}</h2>
       <h2>{multiplicar(10, 2)}</h2>
       <h2>{dividir(10, 2)}</h2>
+      <button className='botao bg-ilson'>TESTE</button>
+      <br />
+      <button className='botao bg-lime-500'>TESTE</button>
     </>
   )
 }
